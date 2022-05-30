6c4997cc22e71a638b5123b4c8b7c1d18f2695a2
xanushkasrivastavax <anushkasrivastavax@gmail.com>
2022-05-30 13:11:35 +0530

    added training

--- app/src/main/java/com/example/loginandsignup/RegisterActivity.kt
+++ app/src/main/java/com/example/loginandsignup/RegisterActivity.kt
@@ -27,7 +27,7 @@ class RegisterActivity : AppCompatActivity() {
         }
         val btn=findViewById<Button>(R.id.btn_signin)
         val text = findViewById<TextView>(R.id.logo)
-        btn.setOnClickListener {
+        btn.setOnClickListener { _ ->
             val queue = Volley.newRequestQueue(this)
             val url = "https://api-smartflo.tatateleservices.com/v1/auth/login"
             val email = findViewById<EditText>(R.id.inputName)
@@ -35,26 +35,28 @@ class RegisterActivity : AppCompatActivity() {
             val stringRequest: StringRequest = object : StringRequest(
                 Request.Method.POST, url,
                 Response.Listener { response ->
-                    text.text = "Response is: ${response.substring(0,5)}"
+                    text.text = "Response is: ${response.substring(0, 5)}"
 //                    Toast.makeText(this, response, Toast.LENGTH_LONG).show()
+                    val bundle = Bundle()
 
+                    bundle.putString("anushka", text.getText().toString())
+                    intent = Intent(this, displaytoken::class.java)
+                    intent.putExtras(bundle)
+                    startActivity(intent)
                 },
                 Response.ErrorListener {
                     text.text = "That didn't work!"
-                })
-            {
-//                override fun getBodyContentType(): String {
-//                    return "application/json"
-//                }
-                override fun getParams(): MutableMap<String, String> {
+                }) {
+
+                        override fun getParams(): MutableMap<String, String> {
                     val params = HashMap<String, String>()
                     params.put("email", email.getText().toString())
                     params.put("password", password.getText().toString())
                     return params;
-                }
-            }
+                }}
             queue.add(stringRequest)
         }
+    }
 
 
 
